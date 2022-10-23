3e3b61518d3eca490cda036846383da37d9380bc
amahatoo1980 <88518562+amahatoo1980@users.noreply.github.com>
2022-10-23 01:58:26 -0400

    test push

--- Cpp-Folder/Tic Tac Toe Game/Driver.cpp
+++ Cpp-Folder/Tic Tac Toe Game/Driver.cpp
@@ -8,4 +8,23 @@
 char square[10] = {'0','1','2','3','4','5','6','7','8','9'};
 
 int checkForWiner();
+void gameBoard();
+
+int main(){
+
+  using namespace std;
+
+
+  int player_one = 1, i, choice;
+
+  char marker;
+
+  do{
+    gameBoard();
+  }while();
+    gameBoard();
+  
+  
+
+}
 
